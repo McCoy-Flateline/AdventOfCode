94d53000386486e550e9dd7047c50b1cd5e6105c
McCoy-Flateline <61716914+McCoy-Flateline@users.noreply.github.com>
2024-12-07 19:59:56 +1300

    Day 7
    Day 6 is still to be attempted. Day 7 involved some interesting maths. If the last value of the list is NOT a multiple of the target then we only need to test the addition. With this info working backwards through the list is optimal.

---
+++ 2024/day_06.py
@@ -0,0 +1,3 @@
+# AdventOfCode/2024/day6/py
+
+from aocd import get_data
---
+++ 2024/day_07.py
@@ -0,0 +1,58 @@
+# AdventOfCode/2024/day7/py
+
+from aocd import get_data
+from math import log10
+
+from typing import List
+
+
+TEST_DATA = open("./test_input/day_07.txt", "r").read()
+RAW_DATA = get_data(day=7, year=2024)
+
+
+def parse_input(raw: str) -> List[List[int]]:
+    data = raw.split("\n")
+    return [list(map(int, line.replace(":", "").split())) for line in data]
+
+
+def is_tractable(target: int, numbers: List[int], allow_concat: bool = True) -> bool:
+    *head, last_number = numbers
+    if not head:
+        return last_number == target
+    full, remainder = divmod(target, last_number)
+    if is_tractable(full, head, allow_concat) and remainder == 0:
+        return True
+    if (
+        allow_concat
+        and endswith(target, last_number)
+        and is_tractable(target // (10 ** digits(last_number)), head, allow_concat)
+    ):
+        return True
+    return is_tractable(target - last_number, head, allow_concat)
+
+
+def digits(n) -> int:
+    return int(log10(n)) + 1
+
+
+def endswith(a: int, b: int) -> bool:
+    return (a - b) % 10 ** digits(b) == 0
+
+
+data = parse_input(RAW_DATA)
+
+part_1 = 0
+for line in data:
+    target, *numbers = line
+    if is_tractable(target, numbers, False):
+        part_1 += target
+
+part_2 = 0
+for line in data:
+    target, *numbers = line
+    if is_tractable(target, numbers, False):
+        part_2 += target
+    elif is_tractable(target, numbers):
+        part_2 += target
+
+print(f"Calibration results: {part_1}\nConcat calibration results: {part_2}")
